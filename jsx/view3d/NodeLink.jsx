f6176ef5c48f5cae2ed1846ffd8c21778b9f3755
Alex Forsythe <awforsythe@gmail.com>
2019-07-25 14:47:36 -0500

    Update NodeLink to display properly, with directional arrow and offset

--- jsx/view3d/NodeLink.jsx
+++ jsx/view3d/NodeLink.jsx
@@ -11,58 +11,65 @@ class NodeLink {
 
     this.root = new THREE.Group();
 
-    this.material = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+    this.material = new THREE.MeshLambertMaterial({ color: 0x112299 });
     this.cylinderGeometry = new THREE.CylinderBufferGeometry(0.05, 0.05, 1.0, 16);
     this.cylinder = new THREE.Mesh(this.cylinderGeometry, this.material);
     this.root.add(this.cylinder);
+    this.cylinder.position.z = -0.1;
     this.cylinder.rotation.z = Math.PI / 2;
 
-    this.matX = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-    this.geoX = new THREE.BoxBufferGeometry(0.1, 0.1, 0.1);
-    this.boxX = new THREE.Mesh(this.geoX, this.matX);
-    this.root.add(this.boxX);
-    this.boxX.position.set(1, 0, 0);
-
-    this.matY = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-    this.geoY = new THREE.BoxBufferGeometry(0.1, 0.1, 0.1);
-    this.boxY = new THREE.Mesh(this.geoY, this.matY);
-    this.root.add(this.boxY);
-    this.boxY.position.set(0, 1, 0);
-
-    this.matZ = new THREE.MeshLambertMaterial({ color: 0x0000ff });
-    this.geoZ = new THREE.BoxBufferGeometry(0.1, 0.1, 0.1);
-    this.boxZ = new THREE.Mesh(this.geoZ, this.matZ);
-    this.root.add(this.boxZ);
-    this.boxZ.position.set(0, 0, 1);
+    this.coneGeometry = new THREE.ConeBufferGeometry(0.15, 0.5, 16, 1);
+    this.cone = new THREE.Mesh(this.coneGeometry, this.material);
+    this.root.add(this.cone);
+    this.cone.position.z = -0.1;
+    this.cone.rotation.z = Math.PI / 2;
   }
 
   setSrcPosition(newPosition) {
     this.srcPosition.copy(newPosition);
-    this.updateMesh();
+    this._updateMesh();
   }
 
   setDstPosition(newPosition) {
     this.dstPosition.copy(newPosition);
-    this.updateMesh();
+    this._updateMesh();
   }
 
-  updateMesh() {
-    const displacement = new THREE.Vector3().copy(this.dstPosition).sub(this.srcPosition);
-    const distance = displacement.length();
+  _updateMesh() {
+    const [forward, distance] = this._getDirectionAndDistance();
+    const [v0, v1, displayDistance] = this._getDisplayPoints(forward, distance);
+    const matrix = this._getRotationMatrix(forward);
+
+    this.root.position.lerpVectors(v0, v1, 0.5);
+    this.root.setRotationFromMatrix(matrix);
+    this.cone.position.x = (displayDistance * -0.5);
+    this.cylinder.scale.y = displayDistance;
+  }
+
+  _getDirectionAndDistance() {
+    const v = new THREE.Vector3().copy(this.dstPosition).sub(this.srcPosition);
+    const distance = v.length();
     if (distance > 0.0) {
-      const halfDisplacement = new THREE.Vector3().copy(displacement).multiplyScalar(0.5);
-      const midpoint = new THREE.Vector3().copy(this.srcPosition).add(halfDisplacement);
-
-      const forward = new THREE.Vector3().copy(displacement).divideScalar(distance);
-      const right = new THREE.Vector3().copy(forward).cross(new THREE.Vector3(0, 1, 0));
-      const up = new THREE.Vector3().copy(right).cross(forward);
-
-      const matrix = new THREE.Matrix4().makeBasis(forward, up, right);
-
-      this.root.position.copy(midpoint).add(new THREE.Vector3().copy(right).multiplyScalar(0.1));
-      this.root.setRotationFromMatrix(matrix);
-      this.cylinder.scale.y = distance;
+      v.divideScalar(distance);
+    } else {
+      v.set(1, 0, 0);
     }
+    return [v, distance];
+  }
+
+  _getDisplayPoints(direction, distance) {
+    const insetDistance = Math.min(1.0, distance * 0.333);
+    const inset = new THREE.Vector3().copy(direction).multiplyScalar(insetDistance);
+
+    const v0 = new THREE.Vector3().copy(this.srcPosition).add(inset);
+    const v1 = new THREE.Vector3().copy(this.dstPosition).sub(inset);
+    return [v0, v1, distance - (insetDistance * 2.0)];
+  }
+
+  _getRotationMatrix(forward) {
+    const right = new THREE.Vector3().copy(forward).cross(new THREE.Vector3(0, 1, 0)).normalize();
+    const up = new THREE.Vector3().copy(right).cross(forward).normalize();
+    return new THREE.Matrix4().makeBasis(forward, up, right);
   }
 }
 
